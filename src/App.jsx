3d8c6dff80735fde55fdddd0ee5b077f41b0d05b
gabrilgvbf <gabrilgvbf@gmail.com>
2023-03-08 22:54:22 -0300

    tabela imc

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,30 @@ function App() {
   const [n1, setn1] = useState(0)
   const [n2, setn2] = useState(0)
 
+
   let calculoimc = Number(n2) / Number(n1 ** 2)
 
   const [calcbutton, setcalcbutton] = useState(false)
+
+  const mensagem = (resultado) => {
+    if (resultado < 18.5) {
+      return 'ABAIXO DO PESO'
+    } else if (resultado >= 18.5 && resultado < 25) {
+      return 'PESO IDEAL'
+    } else if (resultado >= 25 && resultado < 30) {
+      return 'SOBREPESO'
+    } else if (resultado >= 30 && resultado < 35) {
+      return 'OBESIDADE GRAU I'
+    } else if (resultado >= 35 && resultado < 40) {
+      return 'OBESIDADE GRAU II'
+    } else {
+      return 'OBESIDADE GRAU III ou MÓRBIDA'
+    }
+
+  }
+
+  const tabela = mensagem(calculoimc)
+
 
 
 
@@ -18,18 +39,20 @@ function App() {
       <h1>CALCULE SEU IMC</h1>
       <form>
         <label>Digite sua altura:</label>
-        <input type='number' autocomplete="off" onBlur={(e) => setn1(e.target.value)} ></input>
+        <input type='number' onBlur={(e) => setn1(e.target.value)} ></input>
         <label>Digite seu peso:</label>
-        <input type='number' autocomplete="off" onBlur={(e) => setn2(e.target.value)}></input>
+        <input type='number' onBlur={(e) => setn2(e.target.value)}></input>
 
-        <button onClick={(e) => setcalcbutton(!calcbutton)} >CALCULAR</button>
+        <button type='subtmit' onClick={(e) => setcalcbutton(true)} >CALCULAR</button>
         <button className={'apagar'} type='reset' onClick={(e) => setcalcbutton(false)}   >APAGAR</button>
 
         {calcbutton && (<>
 
-          <h2 className={'resultado'}>{isNaN(calculoimc) ? alert('Número invalido') : calculoimc.toFixed(2)}</h2>
+          <h2 className={'resultado'}>{isNaN(calculoimc) ? <span>Número Invalido</span> : calculoimc.toFixed(2)}</h2>
+          <h2>{isNaN(calculoimc) ? <span>---</span> : tabela}</h2>
 
         </>)}
+
       </form>
       <br />
 
